a8fc5741a3460a113e37db93ee7ea7ca022e5c89
Austin Clyde <aclyde@uchicago.edu>
2019-10-27 14:22:58 -0500

    fixed_yaml

--- rldock/environments/utils.py
+++ rldock/environments/utils.py
@@ -16,7 +16,7 @@ class Scorer:
 
     def __init__(self, pdb_file):
         self.receptor = oechem.OEGraphMol()
-        self.score = oedocking.OEScore(oedocking.OEScoreType_Chemscore)
+        self.score = oedocking.OEScore(oedocking.OEScoreType_Shapegauss)
         oedocking.OEReadReceptorFile(self.receptor, pdb_file)
         self.score.Initialize(self.receptor)
 
