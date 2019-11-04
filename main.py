d42d5962b27eb9b7368439e5e6fa0666453e13d5
Austin Clyde <aclyde@uchicago.edu>
2019-11-04 10:19:08 -0600

    fixed_yaml

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ if __name__ == '__main__':
     if args.l is None:
         model = PPO2(CustomPolicy, env, verbose=2, tensorboard_log="tensorlogs/")
     else:
-        pass
-        # model = utils.load_model_with_norm(PPO2, env, args.l)
+        model = utils.load_model_with_norm(PPO2, env, args.l)
     model.learn(total_timesteps=args.e)
     utils.save_model_with_norm(model, env, path=args.s)
     obs = env.reset()
