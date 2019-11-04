43f564521c735628e6befbfbd3c456eb6b662a0f
Austin Clyde <aclyde@uchicago.edu>
2019-11-03 21:14:25 -0600

    fixed_yaml

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 config = {
-    'action_space_d' : 2.5,
+    'action_space_d' : 3,
     'action_space_r' : 5,
     'protein_wo_ligand' : 'resources/lactamase_wo_lignad.pdb',
     'ligand' : 'resources/lactamase_ligand.pdb',
@@ -11,5 +11,5 @@ config = {
     'voxelsize' : 1,
     'output_size' : (20,20,21,16),
     'max_steps' : 25,
-    'decay' : 0.85 # ^25 = 0.001
+    'decay' : 0.8 # ^25 = 0.001
 }
