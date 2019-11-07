2a4d97e0bb48d53d59aa7ac193369e7995e9fe6e
Austin Clyde <aclyde@uchicago.edu>
2019-11-06 20:40:18 -0600

    fixed_yaml

--- rldock/environments/lactamase.py
+++ rldock/environments/lactamase.py
@@ -116,8 +116,8 @@ class LactamaseDocking(gym.Env):
 
     def reset(self, random=True):
         if random:
-            x,y,z, = self.random_space_init.sample().flatten().ravel()  * 0.6
-            x_theta, y_theta, z_theta = self.random_space_rot.sample().flatten().ravel() * 0.35
+            x,y,z, = self.random_space_init.sample().flatten().ravel()  * 1.0
+            x_theta, y_theta, z_theta = self.random_space_rot.sample().flatten().ravel() * 1.0
             self.trans = [x,y,z]
             self.rot = [x_theta, y_theta, z_theta]
             random_pos = self.atom_center.translate(x,y,z)
