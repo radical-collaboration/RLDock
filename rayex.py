272fc340db25a967245d9c28cd1d067c41ca8045
Austin Clyde <aclyde@uchicago.edu>
2019-11-15 17:20:22 -0600

    fixed_yaml

--- rayex.py
+++ rayex.py
@@ -1,3 +1,4 @@
+import numba
 import faulthandler
 import sys
 faulthandler.enable(file=sys.stderr, all_threads=True)
@@ -31,13 +32,13 @@ class MyKerasModel(TFModelV2):
 
         layer_1 = kerasVoxelExtractor(self.inputs)
         ll = tf.keras.layers.BatchNormalization()(layer_1)
-        layer_3p = tf.keras.layers.Dense(128, activation='relu', name='ftp')(ll)
-        layer_4p = tf.keras.layers.Dense(64, activation='relu', name='ftp2')(layer_3p)
-        layer_5p = tf.keras.layers.Dense(64, activation='relu', name='ftp3')(layer_4p)
+        # layer_3p = tf.keras.layers.Dense(128, activation='relu', name='ftp')(ll)
+        # layer_4p = tf.keras.layers.Dense(64, activation='relu', name='ftp2')(layer_3p)
+        layer_5p = tf.keras.layers.Dense(64, activation='relu', name='ftp3')(ll)
 
-        layer_3v = tf.keras.layers.Dense(128, activation='relu', name='ftv')(ll)
-        layer_4v = tf.keras.layers.Dense(64, activation='relu', name='ftv2')(layer_3v)
-        layer_5v = tf.keras.layers.Dense(64, activation='relu', name='ftv3')(layer_4v)
+        # layer_3v = tf.keras.layers.Dense(128, activation='relu', name='ftv')(ll)
+        # layer_4v = tf.keras.layers.Dense(64, activation='relu', name='ftv2')(layer_3v)
+        layer_5v = tf.keras.layers.Dense(64, activation='relu', name='ftv3')(ll)
         layer_out = tf.keras.layers.Dense(
             num_outputs,
             name="my_out",
