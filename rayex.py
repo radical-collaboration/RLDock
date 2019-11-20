440880fd4c94405a0ff58d525f7deb19fccc778c
Austin Clyde <aclyde@uchicago.edu>
2019-11-20 06:30:51 -0600

    fixed_yaml

--- rayex.py
+++ rayex.py
@@ -147,7 +147,7 @@ config['env_config'] = envconf
 config['model'] = {"custom_model": 'keras_model'}
 config['horizon'] = envconf['max_steps'] + 2
 
-trainer = ddpg.ImpalaTrainer(config=config, env='lactamase_docking')
+trainer = ddpg.DDPGTrainer(config=config, env='lactamase_docking')
 # trainer = ppo.PPOTrainer(config=config, env="lactamase_docking")
 # trainer.restore('/homes/aclyde11/ray_results/PPO_lactamase_docking_2019-11-18_13-40-14ihwtk2lw/checkpoint_51/checkpoint-51')
 policy = trainer.get_policy()
