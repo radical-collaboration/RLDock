6dd7bc5b72ef10f12f9c83471f27e83ce4952706
Austin Clyde <aclyde@uchicago.edu>
2019-11-20 16:11:50 -0600

    fixed_yaml

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ discrete = 100.0
 config = {
     'action_space_d' : (19.15 / discrete, 19.51 / discrete, 20.8 / discrete),
     'action_space_r' : (2 * math.pi/ discrete, 2 * math.pi / discrete, 2 * math.pi / discrete),
-    'protein_wo_ligand' : 'resources/lactamase_wo_lignad.pdb',
-    'ligand' : 'resources/lactamase_ligand.pdb',
-    'oe_box' : 'resources/lacatamase.oeb',
+    'protein_wo_ligand' : '/homes/aclyde11/RLDock/resources/lactamase_wo_lignad.pdb',
+    'ligand' : '/homes/aclyde11/RLDock/resources/lactamase_ligand.pdb',
+    'oe_box' : '/homes/aclyde11/RLDock/resources/lacatamase.oeb',
     'bp_dimension': [19.15299892,  19.51399946,  20.80000114],
     'bp_centers' : [ 20.39349937,   7.33399963, -26.39400101],
     'bp_min' : [ 10.81699991,  -2.4230001 , -36.79400158],
@@ -14,8 +14,8 @@ config = {
     'output_size' : (26, 27, 28, 8), # (39,40,42,8),
     'max_steps' : 100,
     'decay' : 0.93, # ^25 = 0.001,
-    'random_ligand_folder' :      'resources/rligands',
-    'random_ligand_folder_test' : 'resources/rligands_eval',
+    'random_ligand_folder' :      '/homes/aclyde11/RLDock/resources/rligands',
+    'random_ligand_folder_test' : '/homes/aclyde11/RLDock/resources/rligands_eval',
     'voxel_method' : 'C',
     'debug' : False
 }
