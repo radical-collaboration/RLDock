3674cd8ecdaf5688b7d925fa72b12a24cdb4c392
Austin Clyde <aclyde@uchicago.edu>
2019-11-20 07:18:53 -0600

    fixed_yaml

--- rayex.py
+++ rayex.py
@@ -8,8 +8,6 @@ from ray.rllib.agents.ppo import ppo
 # from ray.rllib.agents.ppo import appo
 from ray.tune.logger import pretty_print
 from ray.rllib.models import ModelCatalog
-from ray.rllib.agents.ddpg import ddpg
-
 from argparse import ArgumentParser
 from ray import tune
 
@@ -20,53 +18,38 @@ from ray.rllib.models.tf.misc import normc_initializer
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.utils import try_import_tf
 from rldock.voxel_policy.utils_tf2 import lrelu
-from ray.rllib.models.preprocessors import Preprocessor
 
 from rldock.environments.lactamase import  LactamaseDocking
 from resnet import Resnet3DBuilder
 tf = try_import_tf()
 
-class MyPreprocessorClass(TFModelV2):
+class DeepDrug3D(TFModelV2):
     def __init__(self, obs_space, action_space, num_outputs, model_config,
                  name):
-        super(MyPreprocessorClass, self).__init__(obs_space, action_space,
-                                           num_outputs, model_config, name)
-        self.inputs = tf.keras.layers.Input(
-            shape=obs_space.shape, name="observations")
-        layer_14 = Resnet3DBuilder.build_resnet_34(self.inputs, 400)
-        self.model = tf.keras.models.Model(self.inputs, layer_14)
-        self.base_model.load_weights('my_model_weights.h5')
-
-        self.register_variables(self.base_model.variables)
-
-    def forward(self, input_dict, state, seq_lens):
-        model_out, self._value_out = self.base_model(input_dict["obs"])
-        return model_out, state
-
-ModelCatalog.register_custom_model("my_prep", MyPreprocessorClass)
-
-
-class MyKerasModel(TFModelV2):
-    """Custom model for policy gradient algorithms."""
-
-    def __init__(self, obs_space, action_space, num_outputs, model_config,
-                 name):
-        super(MyKerasModel, self).__init__(obs_space, action_space,
+        super(DeepDrug3D, self).__init__(obs_space, action_space,
                                            num_outputs, model_config, name)
         self.inputs = tf.keras.layers.Input(
             shape=obs_space.shape, name="observations")
 
-        layer_14 =  Resnet3DBuilder.build_resnet_34(self.inputs, 400)
+        h = tf.keras.layers.Conv3D(filters=64,  kernel_size=5,padding='valid')(self.inputs)
+        h = tf.keras.layers.LeakyReLU(alpha=0.1)(h)
+        h = tf.keras.layers.Conv3D(64, 3, padding='valid')(h)
+        h = tf.keras.layers.LeakyReLU(alpha=0.1)(h)
+        h = tf.keras.layers.MaxPooling3D(ool_size=(2, 2, 2),
+            strides=None,
+            padding='valid')(h)
+        h = tf.keras.layers.Flatten()(h)
 
 
-        layer_2 = tf.keras.layers.Flatten()(layer_14)
-        layer_3p = tf.keras.layers.Dense(256, activation='relu', name='ftp')(layer_2)
-        layer_4p = tf.keras.layers.Dense(128, activation='relu', name='ftp2')(layer_3p)
+        layer_2 = tf.keras.layers.Flatten()(h)
+        layer_4p = tf.keras.layers.Dense(128, activation='relu', name='ftp2')(layer_2)
         layer_5p = tf.keras.layers.Dense(64, activation=lrelu, name='ftp3')(layer_4p)
 
-        layer_3v = tf.keras.layers.Dense(256, activation='relu', name='ftv')(layer_2)
-        layer_4v = tf.keras.layers.Dense(128, activation='relu', name='ftv2')(layer_3v)
+        layer_4v = tf.keras.layers.Dense(128, activation='relu', name='ftv2')(layer_2)
         layer_5v = tf.keras.layers.Dense(64, activation=lrelu, name='ftv3')(layer_4v)
+
+
+
         layer_out = tf.keras.layers.Dense(
             num_outputs,
             name="my_out",
@@ -79,6 +62,7 @@ class MyKerasModel(TFModelV2):
             activation=None,
             kernel_initializer=normc_initializer(0.1))(layer_5v)
         self.base_model = tf.keras.Model(self.inputs, [layer_out, value_out])
+
         self.base_model.load_weights('my_model_weights.h5')
 
         self.register_variables(self.base_model.variables)
@@ -91,6 +75,7 @@ class MyKerasModel(TFModelV2):
         return tf.reshape(self._value_out, [-1])
 
 
+
 class MyKerasModel(TFModelV2):
     """Custom model for policy gradient algorithms."""
 
@@ -124,32 +109,6 @@ class MyKerasModel(TFModelV2):
             activation=None,
             kernel_initializer=normc_initializer(0.1))(layer_5v)
         self.base_model = tf.keras.Model(self.inputs, [layer_out, value_out])
-
-
-        #
-        # d = torch.load("models/resnet-34-kinetics-cpu.pth")['state_dict']
-        # convs = (list(filter( lambda x : "conv" in x, d.keys())))
-        #
-        # conv1_count = 1
-        # for layer in model.layers:
-        #     if isinstance(layer, tf.keras.layers.Conv3D):
-        #         try:
-        #             py_weight = d[convs.pop(0)].permute((4, 3, 2, 1, 0)).numpy()
-        #             if conv1_count == 1:
-        #                 print("editing weight will take a bit....")
-        #                 py_weight = torch.from_numpy(py_weight).repeat((1,1,1,3,1)).numpy()[:7, :7, :7, :8, :64]
-        #
-        #             # py_bias = d['conv' + str(conv1_count) + ".weight"].numpy()
-        #             print("torch", py_weight.shape)
-        #
-        #             print(layer.get_weights()[0].shape, layer.get_weights()[1].shape)
-        #             conv1_count += 1
-        #             try:
-        #                 layer.set_weights([py_weight, layer.get_weights()[1]])
-        #             except ValueError as e:
-        #                 print(e)
-        #         except IndexError:
-        #             print("no torch", print(layer, layer.get_weights()[0].shape))
 
         self.base_model.load_weights('my_model_weights.h5')
 
@@ -179,41 +138,40 @@ def env_creator(env_config):
     return LactamaseDocking(env_config)  # return an env instance
 register_env("lactamase_docking", env_creator)
 
-config = ddpg.DEFAULT_CONFIG.copy()
+config = ppo.DEFAULT_CONFIG.copy()
 config['log_level'] = 'DEBUG'
 
-# ppo_conf = {"lambda": 0.95,
-#     "kl_coeff": 0.2,
-#     "sgd_minibatch_size": 512,
-#     "shuffle_sequences": True,
-#     "num_sgd_iter": 10,
-#     "lr": 1e-4,
-#     "lr_schedule": None,
-#     "vf_share_layers": False,
-#     "vf_loss_coeff": 0.5,
-#     "entropy_coeff": 0.01,
-#     "entropy_coeff_schedule": None,
-#     "clip_param": 0.3,
-#     "vf_clip_param": 5.0,
-#     "grad_clip": 10.0,
-#     "kl_target": 0.01}
-#
-# config.update(ppo_conf)
+ppo_conf = {"lambda": 0.95,
+    "kl_coeff": 0.2,
+    "sgd_minibatch_size": 512,
+    "shuffle_sequences": True,
+    "num_sgd_iter": 10,
+    "lr": 1e-4,
+    "lr_schedule": None,
+    "vf_share_layers": False,
+    "vf_loss_coeff": 0.5,
+    "entropy_coeff": 0.01,
+    "entropy_coeff_schedule": None,
+    "clip_param": 0.3,
+    "vf_clip_param": 5.0,
+    "grad_clip": 10.0,
+    "kl_target": 0.01}
 
-# config['sample_batch_size'] = 64
-# config['train_batch_size'] = 1024
+config.update(ppo_conf)
+
+config['sample_batch_size'] = 64
+config['train_batch_size'] = 1024
 
 config["num_gpus"] = args.ngpu  # used for trainer process
 config["num_workers"] = args.ncpu
 config['num_envs_per_worker'] = 4
-config['use_state_preprocessor'] = True
 
 config['env_config'] = envconf
-config['model'] = {"custom_model": 'my_prep'}
+config['model'] = {"custom_model": 'keras_model'}
 config['horizon'] = envconf['max_steps'] + 2
 
-trainer = ddpg.DDPGTrainer(config=config, env='lactamase_docking')
-# trainer = ppo.PPOTrainer(config=config, env="lactamase_docking")
+# trainer = impala.ImpalaTrainer(config=config, env='lactamase_docking')
+trainer = ppo.PPOTrainer(config=config, env="lactamase_docking")
 # trainer.restore('/homes/aclyde11/ray_results/PPO_lactamase_docking_2019-11-18_13-40-14ihwtk2lw/checkpoint_51/checkpoint-51')
 policy = trainer.get_policy()
 print(policy.model.base_model.summary())
